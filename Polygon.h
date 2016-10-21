ac5eb3682e103466ad6df7622387a2c9ba7deae2
HugoPourrierNunez <pourrier789@gmail.com>
2016-10-21 14:42:19 +0200

    Premier commit

---
+++ Polygon.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <vector>
+
+struct Point
+{
+	float x;
+	float y;
+	float z;
+};
+typedef struct Point Point;
+
+struct Color
+{
+	float r;
+	float g;
+	float b;
+};
+typedef struct Color Color;
+
+class Polygon
+{
+public:
+	Polygon();
+	~Polygon();
+
+private:
+	std::vector<Point> points;
+};
+
