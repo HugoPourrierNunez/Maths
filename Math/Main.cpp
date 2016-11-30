ad6f29a684eff0b45605631626e84816ca93e3a4
anthonybazelle <a.bazelle1@laposte.net>
2016-11-30 20:51:36 +0100

    Premiere ligne !

--- Math/Main.cpp
+++ Math/Main.cpp
@@ -25,8 +25,10 @@ using namespace std;
 // Variables globales
 EsgiShader g_BasicShader;
 bool g_bDetectLeftClic = false;
-int g_fPosXClic = 0;
-int g_fPosYClic = 0;
+int g_posXClic = 0;
+int g_posYClic = 0;
+int g_lastPosXClic = 0;
+int g_lastPosYClic = 0;
 
 static const float g_Triangle[] = {
 	-0.8f, .8f,
@@ -68,12 +70,61 @@ void animate()
 
 	if (g_bDetectLeftClic == true)
 	{
+		if (g_lastPosXClic == 0 && g_lastPosYClic == 0)
+		{
+			g_lastPosXClic = g_posXClic;
+			g_lastPosYClic = g_posYClic;
+		}
+		else
+		{	
+			// On convertit de l'unité pixel habituelle à l'unité openGL
+			float opGLPosX1 = -0.5f;
+			float opGLPosY1 = 0.f;
+			float opGLPosX2 = 0.5f;
+			float opGLPosY2 = 0.f;
+
+			// On assigne ca au vertex buffer
+			GLfloat g_vertex_buffer_data[] = { opGLPosX1, opGLPosY1, opGLPosX2, opGLPosY2 };
+
+			// Identificateurs des sommets
+			GLuint vertexbuffer;
+
+			// Génère un tampon et place l'identifiant dans 'vertexbuffer'
+			glGenBuffers(1, &vertexbuffer);
+
+			// Les commandes suivantes vont parler de notre tampon 'vertexbuffer'
+			glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
+			// Fournit les sommets à OpenGL.
+			glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+			// premier tampon d'attributs : les sommets
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+			glVertexAttribPointer(
+				0,                   
+				2,                  // taille
+				GL_FLOAT,           // type 
+				GL_FALSE,            
+				0,                  // nombre d'octets séparant deux sommets dans le tampon
+				(void*)0           
+			);
+
+			// Dessine la ligne
+			glDrawArrays(GL_LINES, 0, 2); // Démarre à partir du sommet 0; 2 sommets au total
+
+			glDisableVertexAttribArray(0);
+
+			g_lastPosXClic = g_posXClic;
+			g_lastPosYClic = g_posYClic;
+		}
+		
 		cout << "Click gauche detecte \n" << endl;
 		// Attention ici on récupère des valeurs de x tel que 0 < x < 800 et des valeurs de y tel que 0 < y < 600
-		cout << "X : " << g_fPosXClic << " - Y : " << g_fPosYClic << endl;
+		cout << "X : " << g_posXClic << " - Y : " << g_posYClic << endl;
 		g_bDetectLeftClic = false;
-		g_fPosXClic = 0;
-		g_fPosYClic = 0;
+		g_posXClic = 0;
+		g_posYClic = 0;
 	}
 
 	glVertexAttribPointer(color_location, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), &g_TriangleAttribs[2]);
@@ -117,8 +168,8 @@ void MouseDetect(int button, int state, int x, int y)
 			if (state == GLUT_DOWN)   //Si le bouton gauche est clicker
 				//cout << "Click gauche detecte \n" << endl;  //On affiche un message comme quoi le bouton a ete enfonce
 				g_bDetectLeftClic = true;
-				g_fPosXClic = x;
-				g_fPosYClic = y;
+				g_posXClic = x;
+				g_posYClic = y;
 			break;
 		case GLUT_RIGHT_BUTTON:  //gestion du bouton droit de la souris
 			if (stat == GLUT_DOWN)  //si le bouton est enfonce
